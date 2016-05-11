e062793053ba8d7a82f0d3eb4f0735d1e91853c3
maxim.anurin <dev@zxteam.org>
2016-05-11 02:01:46 +0000

    [Refs #11] TrayIcon + several menu items in ZXAssistCPP

--- src/cpp/org/zxteam/zxassist/host/Main.cpp
+++ src/cpp/org/zxteam/zxassist/host/Main.cpp
@@ -8,26 +8,58 @@
 //*******************************************************//
 
 #include "org/zxteam/lib/reusable/Object.hpp"
-#include "org/zxteam/lib/reusable/app/DesktopApplication.hpp"
+#include "org/zxteam/lib/reusable/Action.hpp"
+#include "org/zxteam/lib/reusable/windows/Application.hpp"
+#include "org/zxteam/lib/reusable/windows/Menu.hpp"
+#include "org/zxteam/lib/reusable/windows/TrayIcon.hpp"
 
-#include "../contract/FeatureHost.hpp"
-#include "ZXAssistApplication.hpp"
-
-#include <iostream>
-#include "boost/intrusive_ptr.hpp"
-#include "org/zxteam/lib/reusable/String.hpp"
-#include "org/zxteam/lib/reusable/runtime/DynamicLibrary.hpp"
-#include "org/zxteam/lib/reusable/fs/FileSystem.hpp"
 #include "Windows.h"
 
+
+//#include "../contract/FeatureHost.hpp"
+//#include "ZXAssistApplication.hpp"
+//#include <iostream>
+//#include "org/zxteam/lib/reusable/runtime/DynamicLibrary.hpp"
+//#include "org/zxteam/lib/reusable/fs/FileSystem.hpp"
+
 using namespace org::zxteam::lib::reusable;
-using namespace org::zxteam::lib::reusable::app;
-using namespace org::zxteam::lib::reusable::runtime;
+using namespace org::zxteam::lib::reusable::windows;
 
-using namespace org::zxteam::zxassist::host;
+//using namespace org::zxteam::zxassist::host;
 
-int main(int argc, wchar_t** argv)
+class MyApplication : public Application
 {
-	ZXAssistApplicationPtr app = new ZXAssistApplication(GetModuleHandle(NULL));
-	return app->Run();
-}
+public:
+	virtual void Run()
+	{
+		typedef Action0Instance<MyApplication> MenuAction;
+		MenuAction onScreenshotAction(this, &MyApplication::OnScreenshot);
+		MenuAction onPreferencesAction(this, &MyApplication::OnPreferences);
+		MenuAction onExitAction(this, &MyApplication::OnExit);
+
+		TrayIcon ti;
+		ti.SetIcon("zxassist_48x48.png");
+		ti.SetToolTip("ZXAssist C++");
+		Menu mainMenu;
+		mainMenu.AddAction("Make Screenshot",  onScreenshotAction);
+		mainMenu.AddAction("Preferences",  onPreferencesAction);
+		mainMenu.AddAction("Exit", onExitAction);
+		ti.SetMenu(mainMenu);
+		
+		Application::Run();
+	}
+private:
+	void OnExit(void) { this->Shutdown(); }
+	void OnScreenshot(void) { MessageBoxA(0, "TODO Make Screenshot", "ZXAssist C++", MB_OK); }
+	void OnPreferences(void) { MessageBoxA(0, "TODO Open Preferences Window", "ZXAssist C++", MB_OK); }
+}/* MyApplication */;
+
+int main(int argc, char** argv)
+{
+	if(TrayIcon::IsTraySubsystemAvailable())
+	{
+		MyApplication app;
+		app.Run();
+	}
+	return 0;
+} 
